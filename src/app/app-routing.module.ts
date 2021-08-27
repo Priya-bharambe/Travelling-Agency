4d48295b8dd571cde0d447f290afde4a63f61fbc
Priya-bharambe <pryabharambe70@gmail.com>
2021-08-27 18:30:16 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { EnquiryComponent } from './enquiry/enquiry.component';
+import { HolidayComponent } from './holiday/holiday.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'enquiry', component:EnquiryComponent},
+  {path:'holiday', component:HolidayComponent},
+  {path:'about', component:AboutComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { EnquiryComponent } from './enquiry/enquiry.component';
+import { HolidayComponent } from './holiday/holiday.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    EnquiryComponent,
+    HolidayComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
